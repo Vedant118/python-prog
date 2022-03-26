4e5c875c6f10ddd501c99e998b8656bc0e3ef7a7
VEDANT THAKUR <78994556+Vedant118@users.noreply.github.com>
2022-03-26 16:29:45 +0530

    Add files via upload

---
+++ check.py
@@ -0,0 +1,9 @@
+x=int(input("Enter the number :- "))
+def check():
+	if(x>0):
+		print("The number is positive")
+	elif(x<0):
+		print("The number is negative")
+	else:
+		print("The number is Zero")
+check()
