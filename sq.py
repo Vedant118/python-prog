6b0888440cddf80f70267761a28d03935c675e15
VEDANT THAKUR <78994556+Vedant118@users.noreply.github.com>
2022-04-07 15:00:43 +0530

    Add files via upload

---
+++ sq.py
@@ -0,0 +1,6 @@
+a=[i**2 for i in range(1,11)]
+print("sq. of 1 to 10",a)
+b=[i**3 for i in range(1,11)]
+print("sq. of 1 to 10",b)
+c=[j for j in a if j%2==0]
+print("Even number if list A an B :- ",c)
