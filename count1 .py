57dc4bb6b0a49d6262f973ae81cb1de7dd89d758
VEDANT THAKUR <78994556+Vedant118@users.noreply.github.com>
2022-04-07 14:21:22 +0530

    Add files via upload

---
+++ count1 .py
@@ -0,0 +1,4 @@
+names = ['piyush','sadhana','megha','vivek','vivek','yagya']
+n= input("Enter name to search ;- ")
+c = names.count(n)
+print("count",c)
---
+++ pass.py
@@ -0,0 +1,10 @@
+pass="Python"
+count = 0
+while (count<3)
+	password=input("Enter password")
+	if pas == password:
+		print("Welcome the Python")
+	break
+else:
+	if (count==3)
+	print("The lime was exceed")
